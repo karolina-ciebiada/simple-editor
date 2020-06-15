c14c660fba4d1c38ca9ffea711acdff250d62b47
Karolina Ciebiada <karolina.ciebiada@icloud.com>
2020-06-15 10:02:25 +0200

    js added

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,26 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+
+const textArea = document.querySelector('.textarea--js');
+const load = document.querySelector('.load--js');
+const save = document.querySelector('.save--js');
+
+const currentValue = localStorage.getItem('entry');
+if (currentValue) {
+    document.querySelector('.info--js').innerHTML = '*';
+}
+
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.setItem('entry', textArea.value);
+    if (textArea.value){
+        document.querySelector(".info--js").innerHTML = "*";
+    } else {
+        document.querySelector(".info--js").innerHTML = "";
+    }
+});
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    textArea.value = localStorage.getItem('entry');
+})
